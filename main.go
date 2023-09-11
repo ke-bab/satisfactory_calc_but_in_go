2f005225da7969d5322db366b70bdda486d970da
Makarenkov Aleksandr <aarenaarenson@gmail.com>
2023-09-11 18:31:16 +0300

    some working code

--- main.go
+++ main.go
@@ -1,11 +1,23 @@
 package main
 
 import (
+	rd "factory-calc/recipe_data"
+	"fmt"
 	"os"
 )
 
 func main() {
+	recipes, err := rd.ExtractData("recipes.json")
+	handleErr(err)
+	found := findLike(recipes, "AluminumCasing")
+	for _, r := range found {
+		minMulti := 60.0 / float32(r.ManufactoringDuration)
+		fmt.Printf("found %q recipe: \n", r.Name)
+		for _, i := range r.Ingredients {
 
+			println(fmt.Sprintf("- x%d %q, %.2f/min", i.Amount, i.Name, float32(i.Amount)*minMulti))
+		}
+	}
 }
 
 func handleErr(err error) {
@@ -13,4 +25,15 @@ func handleErr(err error) {
 		println(err.Error())
 		os.Exit(1)
 	}
+}
+
+func findLike(recipes []rd.Recipe, str string) []rd.Recipe {
+	var found []rd.Recipe
+	for _, r := range recipes {
+		if r.ContainsProduct(str) {
+			found = append(found, r)
+		}
+	}
+
+	return found
 }
--- recipe_data/data_extract.go
+++ recipe_data/data_extract.go
@@ -3,20 +3,65 @@ package recipe_data
 import (
 	"encoding/json"
 	"os"
+	"strconv"
 	"strings"
 )
 
-func ExtractData(filepath string) (*RecipesData, error) {
+func ExtractData(filepath string) ([]Recipe, error) {
 	file, err := os.ReadFile(filepath)
 	if err != nil {
 		return nil, err
 	}
 	var recipeData = RecipesData{}
+	var recipes []Recipe
 	err = json.Unmarshal(file, &recipeData)
 	if err != nil {
 		return nil, err
 	}
-	return &recipeData, nil
+	for _, recipe := range recipeData.Classes {
+		ingredients, err := parseDescription(recipe.Ingredients)
+		if err != nil {
+			return nil, err
+		}
+		products, err := parseDescription(recipe.Products)
+		if err != nil {
+			return nil, err
+		}
+		durationF32, err := strconv.ParseFloat(recipe.ManufactoringDuration, 32)
+		duration := int(durationF32)
+		if err != nil {
+			return nil, err
+		}
+		recipes = append(recipes, Recipe{
+			Name:                  recipe.ClassName,
+			DisplayName:           recipe.DisplayName,
+			Ingredients:           ingredients,
+			Products:              products,
+			ManufactoringDuration: duration,
+			ProducedIn:            recipe.ProducedIn,
+		})
+	}
+
+	return recipes, nil
+}
+
+type Recipe struct {
+	Name                  string
+	DisplayName           string
+	Ingredients           []ResourceDescription
+	Products              []ResourceDescription
+	ManufactoringDuration int
+	ProducedIn            string
+}
+
+func (r Recipe) ContainsProduct(str string) bool {
+	for _, p := range r.Products {
+		if strings.Contains(p.Name, str) {
+			return true
+		}
+	}
+
+	return false
 }
 
 type ResourceDescription struct {
@@ -24,14 +69,19 @@ type ResourceDescription struct {
 	Name   string
 }
 
-func parseDescription(str string) []ResourceDescription {
-	// trim outer braces
+func parseDescription(str string) ([]ResourceDescription, error) {
 	str = trimBraces(str)
-	// split string
-	// parse strings and push to slice
-	// return slice
+	split := splitResourceStrings(str)
+	var splitParsed []ResourceDescription
+	for _, resStr := range split {
+		resDesc, err := parseResourceString(resStr)
+		if err != nil {
+			return []ResourceDescription{}, err
+		}
+		splitParsed = append(splitParsed, resDesc)
+	}
 
-	return []ResourceDescription{}
+	return splitParsed, nil
 }
 
 func trimBraces(str string) string {
@@ -47,8 +97,9 @@ func splitResourceStrings(str string) []string {
 		}
 		ok, l, r := findBracePair(str[pos:])
 		if ok {
-			split = append(split, str[l+pos:r+pos+1])
-			pos = r + pos + 1
+			l, r = l+pos, r+pos // because we want absolute position, not relative
+			split = append(split, str[l:r+1])
+			pos = r + 1
 		} else {
 			break
 		}
@@ -69,4 +120,37 @@ func findBracePair(str string) (bool, int, int) {
 	}
 
 	return true, l, r
+}
+
+func parseResourceString(str string) (ResourceDescription, error) {
+	str = trimBraces(str) // trim outer braces again
+	split := strings.Split(str, ",")
+	itemValue := cutItemNameFromClassDefinition(getValue(split[0]))
+	amountValueInt, err := strconv.Atoi(getValue(split[1]))
+	if err != nil {
+		return ResourceDescription{}, err
+	}
+
+	return ResourceDescription{
+		Amount: amountValueInt,
+		Name:   itemValue,
+	}, nil
+}
+
+func getValue(str string) string {
+	return strings.Split(str, "=")[1]
+}
+
+func cutItemNameFromClassDefinition(str string) string {
+	className := strings.Split(str, ".")[1]
+	bpIndex := strings.Index(className, "BP_")
+	descIndex := strings.Index(className, "Desc_")
+	if bpIndex != -1 {
+		return className[3 : len(className)-4]
+	}
+	if descIndex != -1 {
+		return className[5 : len(className)-4]
+	}
+
+	return className[:len(className)-4]
 }
