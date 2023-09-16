7589b3d8101110c2e675c0eb19eac60c7c6f10c8
Makarenkov Aleksandr <aarenaarenson@gmail.com>
2023-09-16 16:58:45 +0300

    js adding recipe to tree

--- front/main.js
+++ front/main.js
@@ -12,11 +12,118 @@ class Grid {
     getGrid() {
         return this.grid
     }
+
+    /**
+     * @param {?RecipeNode} parentRecipe
+     * @param {RecipeNode} newRecipe
+     */
+    addRecipe(parentRecipe, newRecipe) {
+        let x = 0
+        let y = 0
+        if (parentRecipe !== null) {
+            x = parentRecipe.pos.x
+            y = parentRecipe.pos.y
+        }
+        for (let i = 0; i < newRecipe.ingredients.length; i++) {
+            let newPos = new Position()
+            newPos.x = x + 1
+            newPos.y = y + i
+            newRecipe.ingredients[i].pos = newPos
+        }
+        if (parentRecipe !== null) {
+            this.rearrangeRecipesRecursively(parentRecipe)
+        }
+    }
+
+    /**
+     * @param {RecipeNode} startRecipe
+     */
+    rearrangeRecipesRecursively(startRecipe) {
+        startRecipe.refreshSize()
+        // move lower recipes if any
+        let lower = startRecipe.findRecipesLowerThanThat()
+
+        let mostCloserRecipe = lower.reduce((prev, curr) => prev.pos.y < curr.pos.y ? prev : curr);
+
+        let endCell = startRecipe.pos.y + startRecipe.size
+        let diff = mostCloserRecipe.pos - endCell
+        for (let i = 0; i < lower.length; i++) {
+            mostCloserRecipe.modifyYRecursive(-diff)
+        }
+        //
+        if (startRecipe.parentNode !== null) {
+            this.rearrangeRecipesRecursively(startRecipe.parentNode)
+        }
+    }
+}
+
+class Position {
+    /** @type {number} */
+    x
+    /** @type {number} */
+    y
 }
 
 class RecipeNode {
+    /** @type {number} */
+    size
+    /** @type {Position} */
+    pos
+    /** @type {?RecipeNode} */
+    parentNode
     /** @type {RecipeNode[]} */
-    childNodes;
+    ingredients
+    /** @type {string} */
+    recipe
+    /** @type {Grid} */
+    grid
+
+    constructor(parentNode) {
+        this.parentNode = parentNode
+        this.ingredients = []
+    }
+
+    removeRecipe() {
+        // todo: implement
+    }
+
+    refreshSize() {
+        this.size = 1
+        let sum = 0
+        for (let i = 0; i < this.ingredients.length; i++) {
+            sum += this.ingredients[0].size
+        }
+        if (sum < 1) {
+            this.size = 1
+        } else {
+            this.size = sum
+        }
+    }
+
+    /**
+     * @return {RecipeNode[]}
+     */
+    findRecipesLowerThanThat() {
+        if (this.parentNode === null) {
+            return []
+        }
+        let recipes = []
+        for (let i = 0; i < this.parentNode.ingredients.length; i++) {
+            if (this.parentNode.ingredients[i].pos.y > this.pos.y) {
+                recipes.push(this.parentNode.ingredients[i])
+            }
+        }
+
+        return recipes
+    }
+
+    /**
+     * @param {number} y
+     */
+    modifyYRecursive(y) {
+        this.pos.y += y
+        this.ingredients.forEach((recipe) => recipe.modifyYRecursive(y))
+    }
 }
 
 // ui api
@@ -31,14 +138,17 @@ class RecipeNode {
 // render
 // create cell
 
+
 const width = 6
 const height = 3
 
 let grid = new Grid()
-let v = grid.getGrid()
-grid.setCell(0,0 ,"recipe1")
-grid.setCell(1,0 ,"recipe1")
-grid.setCell(1,3 ,"recipe1")
+
+grid.addRecipe(null, new RecipeNode())
+
+grid.setCell(0, 0, "recipe1")
+grid.setCell(1, 0, "recipe1")
+grid.setCell(1, 3, "recipe1")
 
 render(grid.getGrid())
 
