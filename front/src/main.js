d0f4f7e2f1ce43b0b639d269af51c61553ec7ceb
Makarenkov Aleksandr <aarenaarenson@gmail.com>
2023-09-25 21:04:15 +0300

    react?

--- front/src/main.js
+++ front/src/main.js
@@ -1,3 +1,10 @@
 import {App} from "./app/app";
+import { createRoot } from 'react-dom/client';
+import React from 'react';
+import Button from '@mui/material/Button';
+import {App2} from './App'
+
+const root = createRoot(document.getElementById('app'));
+root.render(<App2 />)
 
 new App().run()
--- webpack.config.js
+++ webpack.config.js
@@ -8,4 +8,21 @@ module.exports = {
         filename: 'main.js',
         path: path.resolve(__dirname + '/front/', 'dist'),
     },
+    module: {
+        rules: [
+            {
+                test: /\.(?:js|mjs|cjs)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            ['@babel/preset-env', { targets: "defaults" }],
+                            ["@babel/preset-react", {"runtime": "automatic"}]
+                        ]
+                    }
+                }
+            }
+        ]
+    },
 };
