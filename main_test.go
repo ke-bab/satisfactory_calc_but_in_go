0e4b48c9d343183d8a5b39077196922b3e04a0ce
Makarenkov Aleksandr <aarenaarenson@gmail.com>
2023-10-13 15:51:37 +0300

    testing bitwise hashmap

--- main_test.go
+++ main_test.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"factory-calc/back/recipe_data"
+	"fmt"
+	"math/rand"
 	"testing"
 )
 
@@ -21,4 +23,74 @@ func TestFindLike(t *testing.T) {
 	if len(notFound) != 0 {
 		t.Errorf("len of notFound slice is not 0")
 	}
+}
+
+func TestLongBitwiseHash(t *testing.T) {
+	max := 300
+	n := 200
+
+	checkMap := createCheckMap(max)
+	unique := make([]int, n)
+	for i := 0; i < n; {
+		r := rand.Intn(max)
+		fmt.Printf("gen %d\n", r)
+		if !exists(checkMap, r) {
+			unique[i] = r
+			fmt.Printf("add %d\n", r)
+			addToCheckMap(&checkMap, r)
+			i++
+		}
+
+	}
+
+	if len(unique) != n {
+		t.Errorf("len of slice is not %d", n)
+	}
+	for i, v := range unique {
+		count := 0
+		for _, v2 := range unique {
+			if v2 == v {
+				count++
+			}
+		}
+		if count > 1 {
+			t.Errorf("found non unique value: %d (index %d)", v, i)
+		}
+	}
+}
+
+func createCheckMap(max int) []int {
+	size := max / 32
+	if max%32 > 0 {
+		size++
+	}
+
+	return make([]int, size)
+}
+
+func exists(checkMap []int, v int) bool {
+	index := findIndex(v)
+	fmt.Printf("index: %d, value: %d, integer: %d\n", index, v, checkMap[index])
+	bin := 1 << (v % 32)
+	return checkMap[index]&bin > 0
+}
+
+func addToCheckMap(checkMap *[]int, v int) {
+	index := findIndex(v)
+	bin := 1 << (v % 32)
+
+	(*checkMap)[index] |= bin
+}
+
+func findIndex(v int) int {
+	if v == 0 {
+		return 0
+	}
+	index := v / 32
+
+	if v%32 == 0 {
+		index--
+	}
+
+	return index
 }
